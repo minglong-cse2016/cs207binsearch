8904369faa7ba7137f6bcd8a2dc493d6f0b7e771
minglong-cse2016 <minglong_wu@g.harvard.edu>
2016-09-29 21:22:39 -0400

    added badges

--- test_binsearch.py
+++ test_binsearch.py
@@ -9,8 +9,29 @@ def test_char():
     with raises(TypeError):
         binary_search(['a', 3], 3)
 
-#def test_mymath():
-#    assert mymedian([9,3, 6]) == 6
+def test_no_input():
+    with raises(TypeError):
+        binary_search()
+
+def test_empty_input():
+    assert binary_search([], 1) == -1
+
+def test_one_input():
+    assert binary_search([5], 10) == -1
+
+def test_two_input():
+    assert binary_search([5, 10], 3) == -1
+    
+def test_left_boundary():
+    assert binary_search([1, 2, 3, 4], -1) == -1
+
+def test_middle():
+    assert binary_search([1, 2, 8, 10], 5) == -1
+
+def test_right_boundary():
+    assert binary_search([1, 2, 3, 4], 10) == -1
+
+    
     
 #def test_zero_median():
 #    with raises(ValueError):
